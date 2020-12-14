36900cf8b393caef35df5a3212241aeead3f38fb
Pedro <you@example.com>
2020-12-14 07:47:36 -0500

    4

--- enstock/settings.py
+++ enstock/settings.py
@@ -135,9 +135,6 @@ DATABASES = {
     }
 }
 
-# Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
