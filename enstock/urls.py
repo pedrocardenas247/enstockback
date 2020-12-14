389c03b29b9f6c9895b13aac7108917e30256bad
Pedro <you@example.com>
2020-12-14 06:08:10 -0500

    4

--- enstock/urls.py
+++ enstock/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from rest_framework_simplejwt import views as jwt_views
 from rest_framework_simplejwt.views import TokenVerifyView
 from django.conf.urls.static import static
@@ -9,6 +10,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', HelloView.as_view(), name='hello'),
+    path('favicon.ico', RedirectView.as_view(url=static.url('/favicon.ico')))
     path('api/token/', MyObtainTokenPairView.as_view(), name ='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name ='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
