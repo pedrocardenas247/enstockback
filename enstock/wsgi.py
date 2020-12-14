36188426cdc0d5c68884fdd35dbb15ae261598af
Pedro <you@example.com>
2020-12-14 07:13:38 -0500

    4

--- enstock/wsgi.py
+++ enstock/wsgi.py
@@ -8,11 +8,8 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
-from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'enstock.settings')
-
-application = get_wsgi_application()
 application = Cling(get_wsgi_application())
